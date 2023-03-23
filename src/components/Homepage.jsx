e4291acf5749dee66cdfd35796684c8c01bb0f65
imshukla10 <ritesh.1923ec1099@kiet.edu>
2023-03-23 13:02:33 +0530

    Done

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Typography, Row, Col, Statistic } from 'antd';
+import { Link } from 'react-router-dom';
+import { useGetCryptosQuery } from '../services/cryptoApi';
+import {News, Cryptocurrencies} from './index';
+const {Title} = Typography;
+
+
+
+const Homepage = () => {
+
+  const {data,isFetching} = useGetCryptosQuery(10);
+
+  const globalStats = data?.data?.stats;
+
+  // console.log(globalStats);
+
+  if(isFetching) return "Loading...";
+
+  return (
+    <>
+      <Title level={2} className='heading'>Global Crypto Stats</Title>
+      <Row>
+        <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
+        <Col span={12}><Statistic title="Total Exchanges" value={globalStats.totalExchanges} /></Col>
+        <Col span={12}><Statistic title="Total Market Cap" value={globalStats.totalMarketCap} /></Col>
+        <Col span={12}><Statistic title="Total 24h Volume" value={globalStats.total24hVolume} /></Col>
+        <Col span={12}><Statistic title="Total Markets" value={globalStats.totalMarkets} /></Col>
+      </Row>
+      <div className='home-heading-container'>
+        <Title level={2} className='home-title'>Top 10 Cryptocurrencies in the World</Title>
+        <Title level={3} className='show-more'><Link to='/cryptocurrencies'>Show More</Link></Title>
+      </div>
+      <Cryptocurrencies simplified/>
+      <div className='home-heading-container'>
+        <Title level={2} className='home-title'>Latest Crypto News</Title>
+        <Title level={3} className='show-more'><Link to='/news'>Show More</Link></Title>
+      </div>
+      <News />
+    </>
+  )
+}
+
+export default Homepage;  
